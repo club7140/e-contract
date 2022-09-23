bcffbecf072cd0921ec94de9b546f4d8c2873c30
miles <miles.huang@muadao.build>
2022-09-23 11:07:57 +0800

    test

--- test/blindbox.js
+++ test/blindbox.js
@@ -16,4 +16,26 @@ contract("blindbox", async (accounts) => {
     let ok = await blindbox.claimStatus.call(accounts[1]);
     assert.equal(ok, true, "account[1] mint failed");
   });
+  
+  it("token0 token uri is blindTokenURI", async () => {
+    let blindbox = await BlindBox.deployed();
+    let token0URI = await blindbox.tokenURI(0);
+    const BlindTokenURI = "https://muadao-nft.s3.ap-southeast-1.amazonaws.com/blindbox_unopen.png";
+    assert.equal(token0URI, BlindTokenURI, "token0 URI is not equal BlindTokenURI");
+  });
+   
+  it("set token0 uri", async () => {
+    let blindbox = await BlindBox.deployed();
+    await blindbox.setTokenURI(0, "token0URI");
+    let token0URI = await blindbox.tokenURI(0);
+    const BlindTokenURI = "https://muadao-nft.s3.ap-southeast-1.amazonaws.com/blindbox_unopen.png";
+    assert.equal(token0URI, BlindTokenURI, "token0 URI is not equal BlindTokenURI");
+  });
+
+  it("open blindbox", async () => {
+    let blindbox = await BlindBox.deployed();
+    await blindbox.setBlindBoxOpened(true);
+    let token0URI = await blindbox.tokenURI(0);
+    assert.equal(token0URI, "token0URI", "token0 URI is not equal token0URI");
+  });
 })
