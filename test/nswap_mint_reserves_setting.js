36e761a9052c6d2801c177a5f92885d39b77d44a
bocai-h <woaichibocai_hxb@s163.com>
2022-11-27 14:39:51 +0800

    test

--- test/nswap_mint_reserves_setting.js
+++ test/nswap_mint_reserves_setting.js
@@ -24,4 +24,27 @@ contract("TurfPlot", async (accounts) => {
     let totalReserves = await turfPlot.totalReserves.call();
     await truffleAssert.reverts(turfPlot.addNswapMintReserves(parseInt(totalReserves) + 1), "The number of shelves exceeds the number of stocks");
   });
+
+  it("nswapMintReserves can't sub by normal user", async () => {
+    let turfPlot = await TurfPlot.deployed();
+    await truffleAssert.reverts(turfPlot.subNswapMintReserves(1, {from: accounts[1]}), "Ownable: caller is not the owner");
+  });
+
+  it("nswapMintReserves can sub by owner", async () => {
+    let turfPlot = await TurfPlot.deployed();
+    let nswapMintReservesBefore = await turfPlot.nswapMintReserves.call();
+    let totalReservesBefore = await turfPlot.totalReserves.call();
+    let num = parseInt(nswapMintReservesBefore);
+    await turfPlot.subNswapMintReserves(num);
+    let nswapMintReservesAfter = await turfPlot.nswapMintReserves.call();
+    let totalReservesAfter = await turfPlot.totalReserves.call();
+    assert.equal(nswapMintReservesAfter, parseInt(nswapMintReservesBefore) - num, "nswapMintReserves has a wrong num");
+    assert.equal(totalReservesAfter, parseInt(totalReservesBefore) + num, "freeMintReserves has a wrong num");
+  });
+
+  it("nswapMintReserves can't sub exceed itself", async () => {
+    let turfPlot = await TurfPlot.deployed();
+    let num = 1
+    await truffleAssert.reverts(turfPlot.subNswapMintReserves(num), "The number of removals exceeds the total");
+  });
 })
