8f903ac0a07f88063e5cee92e9c2012244b69ef2
bocai-h <woaichibocai_hxb@s163.com>
2022-11-27 22:32:12 +0800

    updatte

--- test/nswap_public_mint.js
+++ test/nswap_public_mint.js
@@ -13,12 +13,33 @@ contract("TurfPlot", async (accounts) => {
     await turfPlot.addNswapMintReserves(num);
     let total_mint = 2;
     await turfPlot.nswapPublicMint(total_mint, {value: total_mint * 1e17, from: accounts[1]})
-    let balance = await turfPlot.balanceOf(accounts[1]);
+    let balance = await turfPlot.balanceOf.call(accounts[1]);
     assert.equal(balance, total_mint, "nswapPublicMint failed");
     let nswapTotalMintedData = await turfPlot.nswapTotalMinted.call();
     assert.equal(total_mint, nswapTotalMintedData[0], "nswapTotalMinted has a wrong num");
 
     let nswapUserCanMintNumData = await turfPlot.nswapUserCanMintNum.call(accounts[1]);
     assert.equal(3, nswapUserCanMintNumData[0], "nswapUserCanMintNum has a wrong num");
+  });
+
+  it("can't nswapPublicMint if deposit value not enough", async () => {
+    let turfPlot = await TurfPlot.deployed();
+    let total_mint = 1
+    await truffleAssert.reverts(turfPlot.nswapPublicMint(total_mint, {from: accounts[1]}), "Your deposit value is less than count * price");
+  });
+
+  it("can't nswapPublicMint if exceed the limit", async () => {
+    let turfPlot = await TurfPlot.deployed();
+    let total_mint = 6
+    await truffleAssert.reverts(turfPlot.nswapPublicMint(total_mint, {value: total_mint * 1e17, from: accounts[2]}), "Your claim have exceeded the limit");
+  });
+
+  it("can nswapPublicMint with discount price if in whitelist", async () => {
+    let turfPlot = await TurfPlot.deployed();
+    await turfPlot.addWhitelist([accounts[3]]);
+    let total_mint = 1;
+    await turfPlot.nswapPublicMint(total_mint, {value: total_mint * 5e16, from: accounts[3]})
+    let balance = await turfPlot.balanceOf.call(accounts[3])
+    assert.equal(balance, total_mint, "nswapPublicMint failed");
   });
 })
--- test/withdraw_fee.js
+++ test/withdraw_fee.js
@@ -1,188 +1,27 @@
-const Block = artifacts.require("Block");
+const TurfPlot = artifacts.require("TurfPlot");
 const truffleAssert = require('truffle-assertions');
 
-contract("block", async (accounts) => {
-  it("reservesClaimNFT stag1", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 50, "reservesClaimNFT not work");
-  });
-  it("reservesClaimNFT stag2", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 100, "reservesClaimNFT not work");
-  });
-
-
-  it("reservesClaimNFT stag3", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 150, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag4", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 200, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag5", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 250, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag6", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 300, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag7", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 350, "reservesClaimNFT not work");
-  });
-
-
-  it("reservesClaimNFT stag8", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 400, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag9", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 450, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag10", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 500, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag11", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 550, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag12", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 600, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag13", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 650, "reservesClaimNFT not work");
-  });
-
-
-  it("reservesClaimNFT stag14", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 700, "reservesClaimNFT not work");
-  });
-
-
-  it("reservesClaimNFT stag15", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 750, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag16", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 800, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag17", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 850, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag18", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 900, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag19", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 950, "reservesClaimNFT not work");
-  });
-
-  it("reservesClaimNFT stag20", async () => {
-    let block = await Block.deployed();
-    await block.reservesClaimNFT({from: accounts[0]});
-     
-    let nextTokenID = await block.next_tokenID.call()
-    assert.equal(nextTokenID, 1000, "reservesClaimNFT not work");
-  });
-  
+contract("TurfPlot", async (accounts) => {
   it("mint will produce fee", async () => {
-    let block = await Block.deployed();
-    await block.addWhitelist([accounts[1]]);
-    await block.claimNFT({value: 5e16, from: accounts[1]});
-    await block.claimNFT({value: 1e17, from: accounts[2]});
-    let balance = await web3.eth.getBalance(block.address);
-    assert.equal(balance, 5e16 + 1e17, "mint fee not work");
+    let turfPlot = await TurfPlot.deployed();
+    let num = 20;
+    await turfPlot.addNswapMintReserves(num);
+    let total_mint = 3;
+    await turfPlot.nswapPublicMint(total_mint, {value: total_mint * 1e17, from: accounts[1]})
+    await turfPlot.nswapPublicMint(total_mint, {value: total_mint * 1e17, from: accounts[2]})
+    let balance = await web3.eth.getBalance(turfPlot.address);
+    assert.equal(balance, 2 * total_mint * 1e17, "mint fee not work");
   });
 
   it("normal account can't withdraw fee", async () => {
-    let block = await Block.deployed();
-    await truffleAssert.reverts(block.withdrawFee({from: accounts[1]}), "Ownable: caller is not the owner");
+    let turfPlot = await TurfPlot.deployed();
+    await truffleAssert.reverts(turfPlot.withdrawFee({from: accounts[1]}), "Ownable: caller is not the owner");
   });
 
   it("only owner can withdraw fee", async () => {
-    let block = await Block.deployed();
-    await block.withdrawFee({from: accounts[0]});
-    let balance = await web3.eth.getBalance(block.address);
+    let turfPlot = await TurfPlot.deployed();
+    await turfPlot.withdrawFee({from: accounts[0]});
+    let balance = await web3.eth.getBalance(turfPlot.address);
     assert.equal(balance, 0, "withdraw fee not work");
   });
 })
